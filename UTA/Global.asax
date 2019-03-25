58a883278767d91494321ec068c55ddadecde602
Dejan Cvijetinovic <dejancv9@gmail.com>
2019-03-25 19:36:15 +0100

    Add project files.

---
+++ UTA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UTA.MvcApplication" Language="C#" %>
---
+++ UTA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UTA.Startup))]
+namespace UTA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
