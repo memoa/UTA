d30de16af7e9072d7a7e825d99cb44614e291b43
Dejan Cvijetinovic <dejancv9@gmail.com>
2019-05-06 12:43:56 +0200

    Backend - Implemented Agency Table CRUD, form and validations

---
+++ UTA/Controllers/AgencyTableController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UTA.Models;
+
+namespace UTA.Controllers
+{
+  public class AgencyTableController : Controller
+  {
+    private ApplicationDbContext _context;
+
+    public AgencyTableController()
+    {
+      _context = new ApplicationDbContext();
+    }
+
+    // GET: AgencyTable
+    public ActionResult Index()
+    {
+      var agency = _context.Agencies.ToList();
+
+      return View(agency);
+    }
+
+    public ActionResult New()
+    {
+      var agency = new Agency();
+
+      return View("AgencyTableForm", agency);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var agency = _context.Agencies.SingleOrDefault(d => d.Id == id);
+
+      if (agency == null)
+        return HttpNotFound();
+
+      return View("AgencyTableForm", agency);
+    }
+
+    public ActionResult Save(Agency agency)
+    {
+      if (!ModelState.IsValid)
+        return View("AgencyTableForm", agency);
+
+      if (agency.Id == 0)
+        _context.Agencies.Add(agency);
+      else
+      {
+        var agencyInDb = _context.Agencies.Single(d => d.Id == agency.Id);
+
+        agencyInDb.Name = agency.Name;
+        agencyInDb.Logo = agency.Logo;
+      }
+
+      _context.SaveChanges();
+
+      return RedirectToAction("Index", "AgencyTable");
+    }
+    public ActionResult Delete(int id)
+    {
+      var agency = _context.Agencies.SingleOrDefault(a => a.Id == id);
+
+      if (agency == null)
+        return HttpNotFound();
+
+      _context.Agencies.Remove(agency);
+      _context.SaveChanges();
+
+      return RedirectToAction("Index", "AgencyTable");
+    }
+  }
+}
--- UTA/Models/Agency.cs
+++ UTA/Models/Agency.cs
@@ -8,12 +8,13 @@ namespace UTA.Models {
   public class Agency {
     public int Id { get; set; }
 
-    [Required]
-    [StringLength(50)]
+    [Display(Name = "Agencija")]
+    [Required(ErrorMessage = "Ovo polje je obavezno!")]
+    [StringLength(50, ErrorMessage = "Dozvoljeno je maksimalno 50 karaktera!")]
     public string Name { get; set; }
 
-    [Required]
-    [StringLength(50)]
+    [Required(ErrorMessage = "Ovo polje je obavezno!")]
+    [StringLength(50, ErrorMessage = "Dozvoljeno je maksimalno 50 karaktera!")]
     public string Logo { get; set; }
   }
 }
