eedf9918dec9f1fb4c1470d116b9e52d6831c5ab
Dejan Cvijetinovic <dejancv9@gmail.com>
2019-05-06 22:46:11 +0200

    Removed unnecesarry files

--- UTA/Controllers/ArrangementsController.cs
+++ UTA/Controllers/ArrangementsController.cs
@@ -20,19 +20,7 @@ namespace UTA.Controllers {
       var arrangements = _context.Arrangements
         .Include(a => a.Destination)
         .Include(a => a.Agency).ToList();
-      /*
-      var arrangementsVm = new List<ArrangementViewModel>();
 
-      foreach (var arrangement in arrangements) {
-        var arrangementVm = new ArrangementViewModel {
-          Id = arrangement.Id,
-          Destination = arrangement.Destination,
-          Picture = arrangement.Picture
-        };
-
-        arrangementsVm.Add(arrangementVm);
-      }
-      */
       return View(arrangements);
     }
 
@@ -49,91 +37,6 @@ namespace UTA.Controllers {
         return HttpNotFound();
 
       return View(arrangement);
-    }
-
-    public ActionResult New()
-    {
-      var destinations = _context.Destinations.ToList();
-      var arrangementTypes = _context.ArrangementTypes.ToList();
-      var transportationTypes = _context.TransportationTypes.ToList();
-      var services = _context.Services.ToList();
-
-      var arrangementVM = new ArrangementViewModel {
-        Destinations = destinations,
-        ArrangementTypes = arrangementTypes,
-        TransportationTypes = transportationTypes,
-        Services = services
-      };
-
-      return View("ArrangementForm", arrangementVM);
-    }
-
-    public ActionResult Edit(int id)
-    {
-      Arrangement arrangement = _context.Arrangements.SingleOrDefault(a => a.Id == id);
-
-      if (arrangement == null)
-        return HttpNotFound();
-
-      var arrangementVM = new ArrangementViewModel(arrangement)
-      {
-        Destinations = _context.Destinations.ToList(),
-        ArrangementTypes = _context.ArrangementTypes.ToList(),
-        TransportationTypes = _context.TransportationTypes.ToList(),
-        Services = _context.Services.ToList()
-      };
-
-      return View("ArrangementForm", arrangementVM);
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Save(Arrangement arrangement)
-    {
-      if (!ModelState.IsValid)
-      {
-        var arrangementVM = new ArrangementViewModel(arrangement)
-        {
-          Destinations = _context.Destinations.ToList(),
-          ArrangementTypes = _context.ArrangementTypes.ToList(),
-          TransportationTypes = _context.TransportationTypes.ToList(),
-          Services = _context.Services.ToList()
-        };
-
-        return View("ArrangementForm", arrangementVM);
-      }
-
-      if (arrangement.Id == 0)
-        _context.Arrangements.Add(arrangement);
-      else
-      {
-        var arrangementInDb = _context.Arrangements.Single(a => a.Id == arrangement.Id);
-
-        arrangementInDb.DestinationId = arrangement.DestinationId;
-        arrangementInDb.Description = arrangement.Description;
-        arrangementInDb.ArrangementTypeId = arrangement.ArrangementTypeId;
-        arrangementInDb.TransportationTypeId = arrangement.TransportationTypeId;
-        arrangementInDb.ServiceId = arrangement.ServiceId;
-        arrangementInDb.StayDays = arrangement.StayDays;
-        arrangementInDb.StayNights = arrangement.StayNights;
-        arrangementInDb.Price = arrangement.Price;
-      }
-      _context.SaveChanges();
-
-      return RedirectToAction("Index", "Arrangements");
-    }
-
-    public ActionResult Delete(int id)
-    {
-      var arrangement = _context.Arrangements.SingleOrDefault(a => a.Id == id);
-
-      if (arrangement == null)
-        return HttpNotFound();
-
-      _context.Arrangements.Remove(arrangement);
-      _context.SaveChanges();
-
-      return RedirectToAction("Index", "Arrangements");
     }
   }
 }
